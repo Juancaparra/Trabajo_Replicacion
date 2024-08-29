5bff1210201f2985950e068a385e4ff80f015764
juancaparra <parraospinajuancamilo@gmail.com>
2024-08-29 06:47:41 -0500

    Creacion de getters y setter

--- src/Factura.java
+++ src/Factura.java
@@ -5,5 +5,45 @@ public class Factura {
     private String descripcion;
     private Date fecha;
     private Cliente cliente;
-    private ItemFactura[] items;
+    private ÍtemFactura[] items;
+
+    public int getFolio() {
+        return folio;
+    }
+
+    public void setFolio(int folio) {
+        this.folio = folio;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public ÍtemFactura[] getItems() {
+        return items;
+    }
+
+    public void setItems(ÍtemFactura[] items) {
+        this.items = items;
+    }
 }
