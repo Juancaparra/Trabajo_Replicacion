b60080264f80ac1dba3b84c1b6261677b9fef836
juancaparra <parraospinajuancamilo@gmail.com>
2024-08-29 07:36:19 -0500

    Creacion de metodos de Factura

--- src/Factura.java
+++ src/Factura.java
@@ -1,3 +1,4 @@
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Factura {
@@ -76,6 +77,60 @@ public class Factura {
 
     public void setItems(ÍtemFactura[] items) {
         this.items = items;
+    }
+
+    public void addItemFactura(ÍtemFactura item) {
+        for (int i = 0; i < this.items.length; i++) {
+            if (this.items[i] == null) {
+                this.items[i] = item;
+                break;
+            }
+        }
+    }
+
+    public float calcularTotal() {
+        float total = 0.0f;
+        for (ÍtemFactura item : this.items) {
+            if (item != null) {
+                total += item.calcularImporte();
+            }
+        }
+        return total;
+    }
+
+    public String generarDetalle() {
+        StringBuilder sb = new StringBuilder("Factura Nº: ");
+        sb.append(folio)
+                .append("\nCliente: ")
+                .append(this.cliente.getNombre())
+                .append("\t NIF: ")
+                .append(cliente.getNif())
+                .append("\nDescripción: ")
+                .append(this.descripcion)
+                .append("\n");
+
+        SimpleDateFormat df = new SimpleDateFormat("dd 'de' MMMM, yyyy");
+        sb.append("Fecha Emisión: ")
+                .append(df.format(this.fecha))
+                .append("\n")
+                .append("\n#\tNombre\t$\tCant.\tTotal\n");
+
+        for(ÍtemFactura item: this.items){
+            if(item == null){
+                continue;
+            }
+            sb.append(item)
+                    .append("\n");
+        }
+        sb.append("\nGran Total: ")
+                .append(calcularTotal());
+
+        return sb.toString();
+    }
+
+    @Override
+    public String toString() {
+        return generarDetalle();
     }
 
 }
